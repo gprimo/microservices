aaf5e3d9bca8fc2afc19441741953b3e1532bf26
gprimo <glaucoprimo@gmail.com>
2022-06-09 20:28:18 -0300

    Finish Authentication Module

---
+++ auth-api/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import * as db from "./src/config/db/initialData.js";
+import userRouter from "./src/modules/user/routes/UserRoutes.js";
+import CheckToken from "./src/config/auth/checkToken.js";
+
+const app = express();
+const env = process.env;
+const PORT = env.PORT || 8080;
+
+db.createInitialData();
+
+
+app.get('/api/status', (req,res) => {
+    return res.status(200).json({
+        service: 'Auth-API',
+        status: "up",
+        httpStatus: 200
+    });
+});
+
+app.use(express.json());
+app.use(userRouter);
+
+
+app.listen(PORT, () => {
+    console.info(`server fully started successfuly at port ${PORT}`)
+});
